624e05964f2190838b1346572eb5f8803ba82d25
giovanni <giovanni_ge@hotmai.com>
2023-01-27 17:41:54 -0300

    fix: adicionando alteração na tela de principal

--- boockStore/src/pages/ListProducts/index.tsx
+++ boockStore/src/pages/ListProducts/index.tsx
@@ -2,7 +2,13 @@ import { useContext } from 'react'
 import { BooksContext } from '../../contexts/BooksContext'
 import { dateFormater } from '../../ultis/formatter'
 
-import { ContainerListProducts, TableMain } from './styles'
+import {
+  ContainerListProducts,
+  TableMain,
+  TdUnic,
+  ThUnic,
+  THUnic,
+} from './styles'
 
 export function ListProducts() {
   const { books } = useContext(BooksContext)
@@ -13,12 +19,16 @@ export function ListProducts() {
       <TableMain>
         <tbody>
           <tr>
+            <ThUnic>ID</ThUnic>
             <th>Produto</th>
             <th>Data</th>
           </tr>
           {books.map((item) => {
             return (
               <tr key={item.id}>
+                <TdUnic>
+                  <input type="text" disabled value={item.id} />
+                </TdUnic>
                 <td>{item.description}</td>
                 <td>{dateFormater.format(new Date(item.createdAt))}</td>
               </tr>
--- boockStore/src/pages/ListProducts/styles.ts
+++ boockStore/src/pages/ListProducts/styles.ts
@@ -17,34 +17,52 @@ export const TableMain = styled.table`
     padding: 1.25rem 2rem;
     text-align: left;
     color: ${(props) => props.theme['green-300']};
+    border: 1px solid ${(props) => props.theme.white};
 
     &:first-child {
       border-bottom-left-radius: 6px;
       border-top-left-radius: 6px;
-      border: 1px solid ${(props) => props.theme.white};
     }
 
     &:last-child {
       border-bottom-right-radius: 6px;
       border-top-right-radius: 6px;
-      border: 1px solid ${(props) => props.theme.white};
     }
   }
 
   td {
     padding: 1.25rem 2rem;
     background-color: ${(props) => props.theme['gray-600']};
+    border: 1px solid ${(props) => props.theme.white};
 
     &:first-child {
       border-bottom-left-radius: 6px;
       border-top-left-radius: 6px;
-      border: 1px solid ${(props) => props.theme.white};
     }
 
     &:last-child {
       border-bottom-right-radius: 6px;
       border-top-right-radius: 6px;
-      border: 1px solid ${(props) => props.theme.white};
+    }
+
+    input {
+      color: ${(props) => props.theme['green-500']};
+      background-color: ${(props) => props.theme.white};
+      border: 0;
+      font-weight: bold;
+      max-width: 40px;
+      height: 30px;
+      text-align: center;
     }
   }
+`
+
+export const TdUnic = styled.td`
+  display: flex;
+  justify-content: center;
+`
+
+export const ThUnic = styled.td`
+  display: flex;
+  justify-content: center;
 `
